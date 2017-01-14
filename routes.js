7f7b1a13996dfe261c2f6432d9867da86f4e5188
dallinn <dallinn@users.noreply.github.com>
2017-01-14 10:28:18 -0700

    add crud routes for /

--- routes.js
+++ routes.js
@@ -2,12 +2,22 @@ module.exports = (function() {
     'use strict';
     var router = require('express').Router();
     
-    router.get('/', function(req,res) {
-        var message = req.query.m;
-        res.render('pages/index', {
-            message: message,
+    router.route('/')
+        .get(function (req, res) {
+            var message = req.query.m;
+            res.render('pages/index', {
+                message: message,   
+            });
+        })
+        .post(function (req, res) {
+            res.send('post to /')
+        })
+        .put(function (req, res) {
+            res.send('put to /')
+        })
+        .delete(function (req, res) {
+            res.send('delete to /')
         });
-    });
 
     return router;
 })();
