27bac44c1b769c4357c943bc3d6680cf1150526c
dallinn <dallinn@users.noreply.github.com>
2017-01-14 14:41:17 -0700

    clean up code

--- routes.js
+++ routes.js
@@ -3,17 +3,14 @@ module.exports = (function() {
     var router = require('express').Router();
     var db = require('./db.js');
         db.connect(function(err) {
-            if (err) {
-                console.error('mysql error : ' + err.stack);
-                return;
-            }
+            if (err) console.error('mysql error : ' + err.stack);
             console.log('connected as id ' + db.threadId);
         });
 
     router.route('/')
         .get(function (req, res) {
             db.query('SELECT * FROM todo WHERE completed=0 ORDER BY id DESC; SELECT * from todo WHERE completed=1 ORDER BY id DESC', function(err, results) {
-                if(err){
+                if(err) {
                     throw err;
                 } else {
                     res.render('pages/index', {todos: results[0], completedTodos: results[1]});
@@ -21,19 +18,14 @@ module.exports = (function() {
             });
         })
         .post(function (req, res) {
-            db.query('INSERT INTO todo SET ?', {message: req.body.todo});
+            db.query('INSERT INTO todo SET ?', {message: req.body.todo}, function(err){
+                if (err) throw err;
+            });
             res.redirect('back');
-        })
-        .put(function (req, res) {
-            res.send('put to /')
         })
         .delete(function (req, res) {
             db.query('UPDATE todo SET completed=1 where ?', {id: req.query.id}), function(err){
-                if (err) {
-                    throw err;
-                } else {
-                    res.send(req.query.id);
-                }
+                if (err) throw err;
             };
             res.redirect('back');
         });
